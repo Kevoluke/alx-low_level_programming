37e7d78df04784ed18dd8a7738cd1748365a0275
Kevoluke <kevoluke@gmail.com>
2022-09-27 13:49:47 -0700

    More pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,19 +3,11 @@
 
 
 /**
-
  * _memset - fills a memory block with a constant byte
-
  * @s: address to memory block
-
  * @b: char to be used
-
  * @n: number of bytes to be used
-
- *
-
  * Return: pointer to the memory block
-
  */
 
 
